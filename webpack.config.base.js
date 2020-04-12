59638061891af2dfcc0be58ea55cb5fb3f634313
atsuhan <atsuhan220@gmail.com>
2020-04-12 22:13:22 +0900

    add webgl org sample and glslify

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -8,11 +8,11 @@ const path = require('path');
 // base config
 const SRC = './src';
 const DEST = './public';
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 
 const constants = readConfig(`${SRC}/constants.yml`);
-const { BASE_DIR } = constants;
+const { BASE_DIR, ENTRY } = constants;
 
 // page/**/*.pug -> dist/**/*.html
 const htmlTemplates = routeDataMapper({
@@ -25,10 +25,7 @@ const htmlTemplates = routeDataMapper({
 
 module.exports = {
   // エントリーファイル
-  entry: {
-    'js/script.js': `${SRC}/js/script.js`,
-    'css/style.css': `${SRC}/scss/style.scss`
-  },
+  entry: ENTRY,
   // 出力するディレクトリ・ファイル名などの設定
   output: {
     path: path.resolve(__dirname, DEST + BASE_DIR),
@@ -89,6 +86,11 @@ module.exports = {
       {
         test: /.ya?ml$/,
         loader: 'js-yaml-loader'
+      },
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ['raw-loader', 'glslify-loader']
       }
     ]
   },
